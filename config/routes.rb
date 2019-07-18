b0622f76a0b518e735010f80794ba9de58842544
taro.tanaka <kwnhe231_0411@yahoo.co.jp>
2019-07-18 13:09:01 +0000

    rouresの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
   
-  # get 'messages/create'
-  # get 'messages/destroy'
-  # get 'sessions/new'
-  # get 'sessions/create'
-  # get 'sessions/destroy'
-  # root to: 'tasks#index'
-  
-   get 'login', to: 'sessions#new'
+  get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
   
   get 'signup', to: 'users#new'
   
-  get 'tasks/show'
-  get 'tasks/new'
+ 
   root to: 'tasks#index'
   
   resources :users, only: [:index, :show, :new, :create]   
-  resources :tasks, controller: 'tasks'
-  resources :tasks, only: [:create, :destroy]
+  resources :tasks
+    
 end
